51d23f23d3e711649dcce87d9fe21522d3d38e49
kendranugent <kenugent02@gmail.com>
2024-12-10 17:30:40 -0700

    site plan

---
+++ Personal Site/movies.mjs
@@ -0,0 +1,127 @@
+const movies = [
+	{
+		director: 'Adam L. Wood',
+		url: '',
+		isBasedOn: '',
+		datePublished: '2010',
+		tags: ['Animation', 'Family'],
+		description: 'It revolves around Merliah Summers, a surfing champion who learns she is half-mermaid and sets out for an undersea adventure to rescue her mother.',
+		image: '.images\barbiemermaid.jpeg',
+		Plot: [
+			'Merliah, the young queen of the quick Malibu waves, thinks that she is an ordinary teenager; however, after an unexpected incident during the surfing tournament, she discovers her true identity: she is a graceful mermaid. Now, with her new best friends--the happy bubblegum-pink dolphin, Zuma, and the beautiful mermaids, Kayla and Xylie--they embark on a fascinating underwater adventure to save the ocean kingdom of Oceana, and Merliahs mother, Calissa. The girls need to find three magical items. Will Merliah, the princess of Oceana, accept the great responsibility?'
+		],
+		name: 'Barbie in a Mermaid Tale',
+		Length: '1hr 15m',
+		recipeInstructions: [
+			'Barbie stars as Merliah, a surfer who learns a shocking secret: she is a mermaid! She and her dolphin friend set out for an undersea adventure to rescue her mother, the queen of Oceana.'
+		],
+		age_rating: 'G',
+		rating: 3
+	},
+
+	{
+		director: 'Adam L. Wood',
+		url: '',
+		isBasedOn: '',
+		datePublished: '2010',
+		tags: ['Animation', 'Family'],
+		description: 'It revolves around Merliah Summers, a surfing champion who learns she is half-mermaid and sets out for an undersea adventure to rescue her mother.',
+		image: '.images\barbiemermaid.jpeg',
+		Plot: [
+			'Merliah, the young queen of the quick Malibu waves, thinks that she is an ordinary teenager; however, after an unexpected incident during the surfing tournament, she discovers her true identity: she is a graceful mermaid. Now, with her new best friends--the happy bubblegum-pink dolphin, Zuma, and the beautiful mermaids, Kayla and Xylie--they embark on a fascinating underwater adventure to save the ocean kingdom of Oceana, and Merliahs mother, Calissa. The girls need to find three magical items. Will Merliah, the princess of Oceana, accept the great responsibility?'
+		],
+		name: 'Barbie in a Mermaid Tale',
+		Length: '1hr 15m',
+		recipeInstructions: [
+			'Barbie stars as Merliah, a surfer who learns a shocking secret: she is a mermaid! She and her dolphin friend set out for an undersea adventure to rescue her mother, the queen of Oceana.'
+		],
+		age_rating: 'G',
+		rating: 3
+	},
+
+    {
+		director: 'Adam L. Wood',
+		url: '',
+		isBasedOn: '',
+		datePublished: '2010',
+		tags: ['Animation', 'Family'],
+		description: 'It revolves around Merliah Summers, a surfing champion who learns she is half-mermaid and sets out for an undersea adventure to rescue her mother.',
+		image: '.images\barbiemermaid.jpeg',
+		Plot: [
+			'Merliah, the young queen of the quick Malibu waves, thinks that she is an ordinary teenager; however, after an unexpected incident during the surfing tournament, she discovers her true identity: she is a graceful mermaid. Now, with her new best friends--the happy bubblegum-pink dolphin, Zuma, and the beautiful mermaids, Kayla and Xylie--they embark on a fascinating underwater adventure to save the ocean kingdom of Oceana, and Merliahs mother, Calissa. The girls need to find three magical items. Will Merliah, the princess of Oceana, accept the great responsibility?'
+		],
+		name: 'Barbie in a Mermaid Tale',
+		Length: '1hr 15m',
+		recipeInstructions: [
+			'Barbie stars as Merliah, a surfer who learns a shocking secret: she is a mermaid! She and her dolphin friend set out for an undersea adventure to rescue her mother, the queen of Oceana.'
+		],
+		age_rating: 'G',
+		rating: 3
+	},
+
+
+    {
+		director: 'Adam L. Wood',
+		url: '',
+		isBasedOn: '',
+		datePublished: '2010',
+		tags: ['Animation', 'Family'],
+		description: 'It revolves around Merliah Summers, a surfing champion who learns she is half-mermaid and sets out for an undersea adventure to rescue her mother.',
+		image: '.images\barbiemermaid.jpeg',
+		Plot: [
+			'Merliah, the young queen of the quick Malibu waves, thinks that she is an ordinary teenager; however, after an unexpected incident during the surfing tournament, she discovers her true identity: she is a graceful mermaid. Now, with her new best friends--the happy bubblegum-pink dolphin, Zuma, and the beautiful mermaids, Kayla and Xylie--they embark on a fascinating underwater adventure to save the ocean kingdom of Oceana, and Merliahs mother, Calissa. The girls need to find three magical items. Will Merliah, the princess of Oceana, accept the great responsibility?'
+		],
+		name: 'Barbie in a Mermaid Tale',
+		Length: '1hr 15m',
+		recipeInstructions: [
+			'Barbie stars as Merliah, a surfer who learns a shocking secret: she is a mermaid! She and her dolphin friend set out for an undersea adventure to rescue her mother, the queen of Oceana.'
+		],
+		age_rating: 'G',
+		rating: 3
+	},
+
+
+    {
+		director: 'Adam L. Wood',
+		url: '',
+		isBasedOn: '',
+		datePublished: '2010',
+		tags: ['Animation', 'Family'],
+		description: 'It revolves around Merliah Summers, a surfing champion who learns she is half-mermaid and sets out for an undersea adventure to rescue her mother.',
+		image: '.images\barbiemermaid.jpeg',
+		Plot: [
+			'Merliah, the young queen of the quick Malibu waves, thinks that she is an ordinary teenager; however, after an unexpected incident during the surfing tournament, she discovers her true identity: she is a graceful mermaid. Now, with her new best friends--the happy bubblegum-pink dolphin, Zuma, and the beautiful mermaids, Kayla and Xylie--they embark on a fascinating underwater adventure to save the ocean kingdom of Oceana, and Merliahs mother, Calissa. The girls need to find three magical items. Will Merliah, the princess of Oceana, accept the great responsibility?'
+		],
+		name: 'Barbie in a Mermaid Tale',
+		Length: '1hr 15m',
+		recipeInstructions: [
+			'Barbie stars as Merliah, a surfer who learns a shocking secret: she is a mermaid! She and her dolphin friend set out for an undersea adventure to rescue her mother, the queen of Oceana.'
+		],
+		age_rating: 'G',
+		rating: 3
+	},
+
+
+    {
+		director: 'Adam L. Wood',
+		url: '',
+		isBasedOn: '',
+		datePublished: '2010',
+		tags: ['Animation', 'Family'],
+		description: 'It revolves around Merliah Summers, a surfing champion who learns she is half-mermaid and sets out for an undersea adventure to rescue her mother.',
+		image: '.images\barbiemermaid.jpeg',
+		Plot: [
+			'Merliah, the young queen of the quick Malibu waves, thinks that she is an ordinary teenager; however, after an unexpected incident during the surfing tournament, she discovers her true identity: she is a graceful mermaid. Now, with her new best friends--the happy bubblegum-pink dolphin, Zuma, and the beautiful mermaids, Kayla and Xylie--they embark on a fascinating underwater adventure to save the ocean kingdom of Oceana, and Merliahs mother, Calissa. The girls need to find three magical items. Will Merliah, the princess of Oceana, accept the great responsibility?'
+		],
+		name: 'Barbie in a Mermaid Tale',
+		Length: '1hr 15m',
+		recipeInstructions: [
+			'Barbie stars as Merliah, a surfer who learns a shocking secret: she is a mermaid! She and her dolphin friend set out for an undersea adventure to rescue her mother, the queen of Oceana.'
+		],
+		age_rating: 'G',
+		rating: 3
+	},
+
+]
+
+export default movies
